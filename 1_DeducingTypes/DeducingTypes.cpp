d272b6ba8b8a51e079692351df1167344c05a4c6
Denis Kruchok <denis.kruchok@globant.com>
2022-11-02 00:05:57 +0300

    Added decltype examples

--- 1_DeducingTypes/DeducingTypes.cpp
+++ 1_DeducingTypes/DeducingTypes.cpp
@@ -326,6 +326,193 @@ void DeduceAuto()
 }
 
 
+/*
+*   decltype
+*/
+
+class Widget
+{
+
+};
+
+template<typename T>
+class CustomVector
+{
+public:
+    T& operator[](std::size_t index);
+};
+
+bool f(const Widget& w)     //  decltype(w)  -   const Widget&
+                            //  decltype(f)  -   bool(const Widget&)
+{
+    return true;
+}
+
+void authenticateUser()
+{
+
+}
+
+
+/*
+*   Function that get container, authentificate user and returning index operation result
+*   Here decltype help to deduce returning type (it will return the same type as c[i] will have)
+*   for case std::vector<bool> c[i] will have type std::vector<bool>::reference and not bool
+*   Here auto show that will be used trailing return type (C++11)
+*/
+
+//template<typename Container, typename Index>
+//auto authAndAccess(Container& c, Index i) -> decltype(c[i]) // C++11, work but need to improve
+//{
+//    authenticateUser();
+//    return c[i];
+//}
+
+template<typename Container, typename Index>
+auto authAndAccess(Container& c, Index i) -> decltype(std::forward<Container>(c)[i]) // C++11, final version
+{
+    authenticateUser();
+    return std::forward<Container>(c)[i];
+}
+
+/*
+*   In C++14
+*   we can ommit railing return type "-> decltype(c[i])"
+*   and it will work but not really correct
+*   here auto means type deduce and compilators will return function type from funtion implementation
+*/
+//template<typename Container, typename Index>
+//auto authAndAccess(Container& c, Index i) // have no correct behavior
+//{
+//    authenticateUser();
+//    return c[i]; // returning type deduce from c[i]
+//}
+
+/*
+*   The problem here will be that operator[] for most containers with T type objects return &T
+*   but for templates reference will be ignored
+*
+*   std::deque<int> d;
+*   authAndAccess(d, 5) = 10;
+*
+*   Here we authenticate User, return d[5] and the try to set 10 to it.
+*   It will not work : d[5] will return type  &int,
+*   but auto will discard & (reference part) and function will return type int;
+*   and it will be rvalue, which we try set with value 10. So it will not compile
+*
+*   So to fix it need to use decltype(auto) in returning part of function
+*/
+
+
+//template<typename Container, typename Index>
+//decltype(auto) authAndAccess(Container& c, Index i) // C++14, work but still need to improve
+//{
+//    authenticateUser();
+//    return c[i]; // returning type deduce from c[i]
+//}
+
+/*
+*   Here decltype(auto) means:
+*   auto        -   show that type must be deduced
+*   decltype    -   show that for deducing needs to use decltype rules
+*
+*   decltype(auto) can be used also for variables
+*
+*   Why we want to improve it?
+*
+*   std::deque<std::string> makeStringDeque(); // fabric function
+*
+*   // we try to make copy of fifth element of deque, that will be returned by function makeStringDeque()
+*   auto s = authAndAccess(makeStringDeque(),5)
+*                               ^
+*                               |
+*                             rvalue
+*   So here we should make template function accept lvalu and rvalue.
+*   To make it we will use universal reference &&
+*/
+
+//template<typename Container, typename Index>
+//decltype(auto) authAndAccess(Container&& c, Index i) // C++14, now c - universal reference, but still can be improved
+//{
+//    authenticateUser();
+//    return c[i]; // returning type deduce from c[i]
+//}
+
+/*
+*   Now the problem with missed std::forward<Container>(c)[i]
+*/
+
+template<typename Container, typename Index>
+decltype(auto) authAndAccess(Container&& c, Index i) // C++14 - final version
+{
+    authenticateUser();
+    return std::forward<Container>(c)[i];
+}
+
+
+
+void DecltypeMain()
+{
+    const int i = 0;                //  decltype(i)  -   const int
+
+
+
+    struct Point
+    {
+        int x;                      //  decltype(Point::x)   -   int
+        int y;                      //  decltype(Point::u)   -   int
+    };
+
+    Widget w;                       //  decltype(w)   -   Widget
+
+    if (f(w)) {}                    //  decltype(f(w))  -   bool
+
+    CustomVector<int> CV;           //  decltype(CV)  -   CustomVector<int>
+
+    //if (CV[0] == 0) {}              //  decltype(CV[0]) -   int&
+
+    std::vector<bool> a{ 1 };
+    auto b = a[0];                  //  a[0]    -   std::vector<bool>::reference
+
+    /*
+    *   decltype(auto) can be used also for vaiables
+    */
+
+    const Widget& cw = w;
+
+    auto myWidget1 = cw;            //  myWidget1   -   Widget
+    decltype(auto) myWidget2 = cw;  //  myWidget2   -   const Widget&
+
+    int x = 0;
+    std::cout << GetTypeName<decltype(x)>() << std::endl;   //  int
+    std::cout << GetTypeName<decltype((x))>() << std::endl; // int&
+
+    /*
+    *   decltype(x) : return int
+    *   here the x - name of variable, so the decltype give type of this name
+    *   But for Lvalue-expression, more complex that name, decltype give garanty, that
+    *   returning type will be always lvalue-reference, that's why in case     *
+    *   decltype((x)) - will return int&
+    *
+    *   So in C++14 it important attentively use with function returning decltype(auto)
+    *
+    *   decltype(auto) f1()
+    *   {
+    *       int x = 0;
+    *       ...
+    *       return x;   // decltype(x) - int, so f1 will return int
+    *   }
+    *
+    *   decltype(auto) f2()
+    *   {
+    *       int x = 0;
+    *       ...
+    *       return (x);   // decltype((x)) - int&, so f1 will return int&
+    *   }
+    */
+}
+
+
 int main()
 {
     //Case1();
@@ -333,8 +520,9 @@ int main()
     //Case3();
 
     //ArgumentsArrays();
-
     //ArgumentsFunctions();
 
-    DeduceAuto();
+    //DeduceAuto();
+
+    DecltypeMain();
 }
